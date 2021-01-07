26ecdfdcd391da80bb82de0e8f6926a690275a53
Henrik <web@villprogrammera.nu>
2021-01-07 16:00:40 +0100

    Initial Solution Setup

---
+++ WebShop/WebShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebShop.WebUI.Startup))]
+namespace WebShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
