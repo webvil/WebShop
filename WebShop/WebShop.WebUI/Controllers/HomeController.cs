809a2e96b125c1c68ff5c96aa9dd98604e1d705d
Henrik <web@villprogrammera.nu>
2021-01-14 11:10:56 +0100

    add stylinng

--- WebShop/WebShop.WebUI/Controllers/HomeController.cs
+++ WebShop/WebShop.WebUI/Controllers/HomeController.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebShop.Core.Contracts;
+using WebShop.Core.Models;
 
 namespace WebShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+        public HomeController(IRepository<Product> productContext,
+          IRepository<ProductCategory> productCategories)
+        {
+            context = productContext;
+            this.productCategories = productCategories;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
         }
 
         public ActionResult About()
