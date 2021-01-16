a27fd2c778171f641bb61458b340ec513b7f5be0
Henrik <web@villprogrammera.nu>
2021-01-16 23:29:09 +0100

    redo migrations

--- WebShop/WebShop.DataAccess.SQL/Migrations/202101160849263_Initial.Designer.cs
+++ WebShop/WebShop.DataAccess.SQL/Migrations/202101160849263_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace WebShop.DataAccess.SQL.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.4")]
-    public sealed partial class AddBasket : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(AddBasket));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "202101151710312_AddBasket"; }
+            get { return "202101160849263_Initial"; }
         }
         
         string IMigrationMetadata.Source
---
+++ WebShop/WebShop.DataAccess.SQL/Migrations/202101160849263_Initial.cs
@@ -0,0 +1,85 @@
+﻿namespace WebShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BasketItems",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        BasketId = c.String(maxLength: 128),
+                        ProductId = c.String(),
+                        Quantity = c.Int(nullable: false),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Baskets", t => t.BasketId)
+                .Index(t => t.BasketId);
+            
+            CreateTable(
+                "dbo.Baskets",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.ProductCategories",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Category = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(maxLength: 20),
+                        Description = c.String(),
+                        Category = c.String(),
+                        Image = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.ShoppingCartItems",
+                c => new
+                    {
+                        ShoppingCartItemId = c.Int(nullable: false, identity: true),
+                        Amount = c.Int(nullable: false),
+                        ShoppingCartId = c.String(),
+                        Product_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.ShoppingCartItemId)
+                .ForeignKey("dbo.Products", t => t.Product_Id)
+                .Index(t => t.Product_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ShoppingCartItems", "Product_Id", "dbo.Products");
+            DropForeignKey("dbo.BasketItems", "BasketId", "dbo.Baskets");
+            DropIndex("dbo.ShoppingCartItems", new[] { "Product_Id" });
+            DropIndex("dbo.BasketItems", new[] { "BasketId" });
+            DropTable("dbo.ShoppingCartItems");
+            DropTable("dbo.Products");
+            DropTable("dbo.ProductCategories");
+            DropTable("dbo.Baskets");
+            DropTable("dbo.BasketItems");
+        }
+    }
+}
