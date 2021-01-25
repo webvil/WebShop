1ee8a84457df144e1d90a6534a394b802209fb74
Codecoala81 <62188788+Codecoala81@users.noreply.github.com>
2021-01-25 13:16:59 +0100

    Added Brand to Product/Index - by Andreas

---
+++ WebShop/WebShop.DataAccess.SQL/Migrations/202101251147385_ProductsOnSale.cs
@@ -0,0 +1,20 @@
+﻿namespace WebShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ProductsOnSale : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.ProductOnSales", newName: "ProductsOnSale");
+            AlterColumn("dbo.ProductsOnSale", "Discount", c => c.Decimal(precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.ProductsOnSale", "Discount", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            RenameTable(name: "dbo.ProductsOnSale", newName: "ProductOnSales");
+        }
+    }
+}
